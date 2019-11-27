df6ac0b6c01ef4e0201176771cf87f476a324240
javier <jrmajano@gmail.com>
2019-11-27 19:26:33 +0100

    geocode, sign in, map style

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
+const axios = require('axios');
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
@@ -28,46 +29,61 @@ router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
   const refugio = req.body.refugio;
-  const localizacion = req.body.localizacion;
-
-  //comprueba que los campos tengan texto
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
-    return;
-  }
+  const localizacion = req.body.address;
+  
 
 
-  User.findOne({ username }, "username", (err, user) => {
+  //llamada axios a la api de google
     
-    //Comprueba que el usuario sea nuevo
-    if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
-      return;
-    }
-
-    //encriptacion password
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
-
-    //plantilla nuevo usuario
-    const newUser = new User({
-      username,
-      password: hashPass,
-      refugio,
-      localizacion
+    console.log('---------------------->estamos aqui-------------------->')
+      
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${localizacion}&key=AIzaSyBrPNbJOlFtyYOkm722n_jbRGtSxOsE_q8`)
+      .then (responseFromAPI => {
+        let coordinates = responseFromAPI.data.results[0].geometry.location
+        console.log(coordinates)
+        //comprueba que los campos tengan texto
+        
+        if (username === "" || password === "") {
+          // res.render("auth/signup", { message: "Indicate username and password" });
+          return;
+        }
+        
+        
+        User.findOne({ username }, "username", (err, user) => {
+          
+          //Comprueba que el usuario sea nuevo
+          if (user !== null) {
+            res.render("auth/signup", { message: "The username already exists" });
+            return;
+          }
+          
+          //encriptacion password
+          const salt = bcrypt.genSaltSync(bcryptSalt);
+          const hashPass = bcrypt.hashSync(password, salt);
+          
+          //plantilla nuevo usuario
+          const newUser = new User({
+            username,
+            password: hashPass,
+            refugio,
+            localizacion,
+            coordinates
+            
+          });
+          
+          //salva el nuevo usuario
+          newUser.save()
+          .then(() => {
+            res.redirect("/");
+          })
+          .catch(err => {
+            res.render("auth/signup", { message: "Something went wrong" });
+          })
+        });
+      })
+      .catch (error => console.log(error))       
     });
-
-    //salva el nuevo usuario
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
-  });
-});
-
+    
 router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
