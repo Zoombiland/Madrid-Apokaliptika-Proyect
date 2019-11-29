54917f82b742cb240dc4a02a683d4f7c4e485cca
USUARIO <usuario@Macs-MacBook-Pro.local>
2019-11-29 12:53:14 +0100

    update 29/11/2019 13:00

--- routes/index.router.js
+++ routes/index.router.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+  let data = {layout: false}
+  res.render('index', data)
+}
+  );
 // router.use((req, res, next) => {
 //   console.log(req.session)
 //   req.session.currentUser ? next() : res.redirect("/login")
